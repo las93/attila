dd384c52a35f2adba99b4b237d20c8970ca49188
las93 <las92i@hotmail.fr>
2015-06-04 00:30:59 +0200

    Bug fixed on join + add unique

--- Attila/Batch/Entity.php
+++ Attila/Batch/Entity.php
@@ -320,6 +320,7 @@ class Entity
 					$sQuery = 'CREATE TABLE IF NOT EXISTS '.SQL_FIELD_NAME_SEPARATOR.$sTableName.SQL_FIELD_NAME_SEPARATOR.' (';
 
 					$aIndex = array();
+					$aUnique = array();
 					$aPrimaryKey = array();
 
 					foreach ($oOneTable->fields as $sFieldName => $oOneField) {
@@ -361,6 +362,7 @@ class Entity
 
 						if (isset($oOneField->key) && $oOneField->key === 'primary') { $aPrimaryKey[] = $sFieldName; }
 						else if (isset($oOneField->key) && $oOneField->key === 'index') { $aIndex[] = $sFieldName; }
+						else if (isset($oOneField->key) && $oOneField->key === 'unique') { $aUnique[] = $sFieldName; }
 					
     					if (isset($oOneField->join) && is_string($oOneField->join)) {
 
@@ -388,10 +390,20 @@ class Entity
 					if (count($aPrimaryKey) > 0) { $sQuery .= 'PRIMARY KEY('.implode(',', $aPrimaryKey).') , '; }
 					
 					if (count($aIndex) > 0) { $sQuery .= 'KEY('.implode(',', $aIndex).') , '; }
+					
+					if (count($aIndex) > 0) { $sQuery .= 'UNIQUE KEY '.$aIndex[0].' ('.implode(',', $aIndex).') , '; }
 
 					if (isset($oOneTable->index)) {
 
 						foreach ($oOneTable->index as $sIndexName => $aFields) {
+
+							$sQuery .= 'KEY '.$sIndexName.' ('.implode(',', $aFields).') , ';
+						}
+					}
+
+					if (isset($oOneTable->unique)) {
+
+						foreach ($oOneTable->unique as $sIndexName => $aFields) {
 
 							$sQuery .= 'KEY '.$sIndexName.' ('.implode(',', $aFields).') , ';
 						}
@@ -694,7 +706,7 @@ class '.$sTableName.' extends Entity
     							}
     			                     
     							$sContentFile .= ' = $oOrm->where($aWhere)
-						           ->load(false, \''.ENTITY_NAMESPACE.'\');';
+						           ->load(false, \''.ENTITY_NAMESPACE.'\\\\\');';
 
     							if ((!isset($oField->key) || (isset($oField->key) && $oField->key != 'primary' 
     							 && (is_array($oField->key) && !in_array('primary', $oField->key))))
